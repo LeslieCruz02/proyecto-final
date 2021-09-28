c869350d256b9c8c532f7d7bc686b32f9fd3f148
Daniel Gomez <danielrozo11.49@gmail.com>
2021-09-28 09:05:15 -0500

    nuevo backend

--- Express/model-products.js
+++
@@ -1,6 +0,0 @@
-products = [{"nombre": "papa", "precio": 2000}, {"nombre": "arroz", "precio": 1900},
-{"nombre": "platano", "precio": 900}, {"nombre": "cebolla", "precio": 1200}];
-
-module.exports = {
-  products
-}
