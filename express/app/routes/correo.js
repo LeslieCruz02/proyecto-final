1b095656a0b7933b6ade47cf22c90bf9abe07afc
Daniel Gomez <danielrozo11.49@gmail.com>
2021-10-22 09:43:45 -0500

    Registro con envio de corre junto con imageness funcionando

--- express/app/routes/correo.js
+++ express/app/routes/correo.js
@@ -19,9 +19,37 @@ const sendMail= async () =>{
     const transporter = createTrans()
     const info = await transporter.sendMail({
         from: "info.petsworldaxm@gmail.com",
-        to: ["fikyhr@gmail.com","danielrozo11.49@gmail.com"], //Se pueden pasar la lista de correos por medio de un array
+        to: "danielrozo11.49@gmail.com", //Se pueden pasar la lista de correos por medio de un array
         subject: "Te saluda Pets World",
-        html: "<b>EL envio fue exitoso, lo logramos</b>"
+        html: 
+        `
+        <table border="0" cellpadding="0" cellspacing="0" width="600px" background-color="#3CACAE" bgcolor="#3CACAE" text-aling:"center">
+        <tr height="250px">  
+            <td bgcolor="" width="600px">
+            <a href='https://postimg.cc/BLQXq4Rz' target='_blank'><img src='https://i.postimg.cc/BLQXq4Rz/Logo.png' border='0' alt='Logo' style="float:left"/></a>
+                <h1 style="color: #fff; text-align:center">Bienvenido</h1>
+                <p  style="color: #fff; text-align:center">
+                    <span style="color: #ffffff">Leslie</span> 
+                    a Pets World
+                </p>
+            </td>
+        </tr>
+        <tr bgcolor="#fff">
+            <td style="text-align:center">
+                <p style="color: #000">¡El mundo de las mascotas a tu disposición!</p>
+            </td>
+        </tr>
+        <tr bgcolor="#fff">
+            <td style="text-align:center">
+                <p>Para confirmar tu cuenta, ingresa al siguiente enlace:</p>
+                <a href="http://localhost:10101/activacion?id=24"
+                    target="_blank"
+                >Confirmar Cuenta</a>
+            </td>
+        </tr>
+        </table>
+    
+    `
     })
     console.log("Mensaje sent: %s",info.messageId);
 
